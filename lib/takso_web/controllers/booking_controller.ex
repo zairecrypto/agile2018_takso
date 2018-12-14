c2ebacf151454080e854b441a24e6c4913705f54
Teddy Mabulay <temabu@ttu.ee>
2018-12-14 13:39:27 +0200

    Bookings table created

--- lib/takso_web/controllers/booking_controller.ex
+++ lib/takso_web/controllers/booking_controller.ex
@@ -1,13 +1,19 @@
 defmodule TaksoWeb.BookingController do
   use TaksoWeb, :controller
 
+  alias Takso.Booking
+
   def new(conn, _params) do
-    render conn, "new.html"
+    render conn, "new.html", changeset: Booking.changeset(%Booking{}, %{})
+  end
+
+  def index(conn, _params) do
+    render conn, "index.html"
   end
 
   def create(conn, _params) do
     conn
     |> put_flash(:info, "Your taxi will arrive in 5 minutes")
-    |> redirect(to: booking_path(conn, :new))
+    |> redirect(to: booking_path(conn, :index))
   end
 end
