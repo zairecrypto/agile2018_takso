e346fcf03d2186a7aa22bfc5000273d05ca17a5f
Teddy Mabulay <temabu@ttu.ee>
2018-12-14 05:30:32 +0200

    first commit

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :takso,
+  ecto_repos: [Takso.Repo]
+
+# Configures the endpoint
+config :takso, TaksoWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "35Xv7TQuQymf4FPgKn2rYMHRMRu8wULRTmgjNH4t4H2QwNIr5qFTm23RN/jqKGP0",
+  render_errors: [view: TaksoWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Takso.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:user_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
